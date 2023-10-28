65934990c8e3e9aa43038e823c18d33369120e42
Cliffano Subagio <cliffano@gmail.com>
2023-10-28 18:14:19 +1100

    Add unit test for logger.

---
+++ tests/test_logger.py
@@ -0,0 +1,16 @@
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,duplicate-code,too-many-locals
+import logging
+import unittest
+from awstaga.logger import init
+
+class TesLogger(unittest.TestCase):
+
+    def test_init(self):
+
+        logger = init(False)
+        assert isinstance(logger, logging.LoggerAdapter) is True
+
+    def test_init_with_enabled_dry_run(self):
+
+        logger = init(True)
+        assert isinstance(logger, logging.LoggerAdapter) is True
