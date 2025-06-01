b653246e506e8bb1235eb38efc5ed5e6f2f89f54
Cliffano Subagio <cliffano@gmail.com>
2025-06-01 18:17:02 +1000

    Standardise example script to use printf instead of echo.

--- examples/awstaga-cli.sh
+++ examples/awstaga-cli.sh
@@ -3,30 +3,26 @@ set -o nounset
 
 export AWS_DEFAULT_REGION=ap-southeast-2
 
-echo "\n\n========================================"
-echo "Show help guide: awstaga --help"
+printf "\n\n========================================\n"
+printf "Show help guide: awstaga --help\n"
 awstaga --help
 
-echo "\n\n========================================"
-echo "Run command with default config file: awstaga"
+printf "\n\n========================================\n"
+printf "Run command with default config file: awstaga\n"
 awstaga
 
-echo "\n\n========================================"
-echo "Run command with specified config file:"
-echo "awstaga --conf-file awstaga.yaml"
+printf "\n\n========================================\n"
+printf "Run command with specified config file:\n"
 awstaga --conf-file awstaga.yaml
 
-echo "\n\n========================================"
-echo "Run command with specified config file and AWS_DEFAULT_REGION env var:"
-echo "AWS_DEFAULT_REGION=ap-southeast-2 awstaga --conf-file awstaga.yaml"
+printf "\n\n========================================\n"
+printf "Run command with specified config file and AWS_DEFAULT_REGION env var:\n"
 awstaga --conf-file awstaga.yaml
 
-echo "\n\n========================================"
-echo "Run command with specified config file in dry-run mode:"
-echo "awstaga --conf-file awstaga.yaml --dry-run"
+printf "\n\n========================================\n"
+printf "Run command with specified config file in dry-run mode:\n"
 awstaga --conf-file awstaga.yaml --dry-run
 
-echo "\n\n========================================"
-echo "Run command with specified config file in dry-run mode with custom batch size:"
-echo "awstaga --conf-file awstaga.yaml --dry-run"
+printf "\n\n========================================\n"
+printf "Run command with specified config file in dry-run mode with custom batch size:\n"
 awstaga --conf-file awstaga.yaml --dry-run --batch-size 50
